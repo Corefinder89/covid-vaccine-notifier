f5e3f81170b43e576fb1bfa02975186799594b02
Corefinder89 <soumyajit.basu1988@outlook.com>
2021-05-02 21:38:29 +0530

    Write code to generate indian proxy 	modified:   Pipfile 	modified:   Pipfile.lock 	modified:   app/datacollector.py 	new file:   requirements.txt

--- app/datacollector.py
+++ app/datacollector.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
 import requests
 import os
+import random
+from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
 
 
 class Datacollector:
     def collector(self):
+        req_proxy = RequestProxy()  # you may get different number of proxy when  you run this at each time
+        proxies = req_proxy.get_proxy_list()  # th
+        ind_proxies = []  # int is list of Indian proxy
+        for proxy in proxies:
+            if proxy.country == 'India':
+                ind.append(proxy.get_address())
+
         try:
             # public endpoint of cowin to find centres using pin code
             endpoint = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin"
@@ -14,16 +23,17 @@ class Datacollector:
                 "date": "31-03-2021"
             }
 
-            payload = ""
+            proxy = {
+                "http": "http://"+str(random.choice(ind_proxies))
+            }
+
             headers = {
                 "content-type": "application/json",
                 "Accept-language": "hi_IN"
             }
 
-            response = requests.request("GET", endpoint, data=payload, headers=headers, params=querystring)
-            # print(response.status_code)
-            # print(response.json())
-            print(response.content.decode())
+            response = requests.request("GET", endpoint, headers=headers, params=querystring, proxies=proxy)
+            print(response.json())
         except Exception as e:
             print(e)
 
