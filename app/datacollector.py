c143e637c0efb99b4dbd2d20bccf0a94ec160ae4
Corefinder89 <soumyajit.basu1988@outlook.com>
2021-05-02 13:44:20 +0530

    First commit 	new file:   .github/workflows/vaccine-notifier.yml 	new file:   .gitignore 	new file:   Pipfile 	new file:   Pipfile.lock 	new file:   app/__init__.py 	new file:   app/datacollector.py 	new file:   app/notifier.py

---
+++ app/datacollector.py
@@ -0,0 +1,33 @@
+import requests
+import os
+
+
+class Datacollector:
+    def collector(self):
+        try:
+            # public endpoint of cowin to find centres using pin code
+            endpoint = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin"
+
+            querystring = {
+                "pincode": os.getenv("pincode"),
+                "date": "31-03-2021"
+            }
+
+            payload = ""
+            headers = {
+                "content-type": "application/json",
+                "Accept-language": "hi_IN"
+            }
+
+            response = requests.request("GET", endpoint, data=payload, headers=headers, params=querystring)
+
+            print(response.json())
+        except KeyError:
+            print("Key not available")
+
+
+d = Datacollector()
+d.collector()
+
+
+
